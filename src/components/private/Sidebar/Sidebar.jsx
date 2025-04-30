c70f1c933175cd4c7d3f15faad42d74c7d31d67c
christopher-lee <clee651@terpmail.umd.edu>
2025-04-30 09:46:53 -0400

    Add mobile hamburger menu.

--- src/components/private/Sidebar/Sidebar.jsx
+++ src/components/private/Sidebar/Sidebar.jsx
@@ -1,11 +1,39 @@
 
+import { useState } from "react";
 import styles from "./Sidebar.module.css";
+import { useMediaQuery } from "react-responsive";
+import Hamburger from "hamburger-react";
 
 function Sidebar({children}) {
+    const [showNav, setShowNav] = useState(false);
+    const isMobile = useMediaQuery({
+        query: '(max-width: 600px)',
+    });
+
+    function handleNavBtn() {
+        setShowNav(prevNav => !prevNav);
+    }
+
     return (
-        <div id="sidebar" className={styles.sidebar}>
-            {children}
-        </div>
+        <>
+        {isMobile ?
+            
+            showNav ?
+            <div id="sidebar" className={styles.sidebar}>
+                <button className={styles.closeNav} onClick={handleNavBtn}>X</button>
+                {children}
+            </div>
+            :
+            // <button className={styles.hamBtn} onClick={handleNavBtn}>Show Nav</button>
+            <Hamburger className={styles.hamBtn} toggled={showNav} toggle={handleNavBtn} />
+
+        :
+            <div id="sidebar" className={styles.sidebar}>
+                {children}
+            </div>
+        }
+        </>
+
     );
 }
 
